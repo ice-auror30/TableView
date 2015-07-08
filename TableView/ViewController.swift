4b7437bde0c65911397af4fb0d0ed814d67b82ce
svat30 <shikhar.sameer@gmail.com>
2015-07-08 22:46:59 +0530

    first commit

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var row_array = [" Ganga ", " Jamuna ", " Saraswati ", " Ravi ", " Beas ", " Chenab "]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return row_array.count
+    
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = row_array[indexPath.row]
+        
+        return cell
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
